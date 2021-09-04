7578516024d1eaba8866e507d43e170febf25527
TonyWu20 <tony.w21@gmail.com>
2021-09-04 15:31:12 +0800

    character arrays and exercises

--- 1-09_character_arrays/exer_1-16.c
+++ 1-09_character_arrays/exer_1-16.c
@@ -12,21 +12,36 @@ int main()
     char line[MAXLINE];    /*current input line*/
     char longest[MAXLINE]; /* longest line saved here */
     max = 0;
+    int c;
     while ((len = my_getline(line, MAXLINE)) > 0)
+    {
+        if (line[len - 1] !=
+            '\n') /* When len = MAXLINE, but the last char is not '\n', the next
+                     result of my_getline succeeds this result */
+            while ((c = getchar()) != EOF && c != '\n')
+                ++len;
         if (len > max)
         {
             max = len;
             copy(longest, line);
         }
+    }
     if (max > 0) /* there was a line */
-        printf("%s", longest);
+    {
+        if (max > 0)
+        {
+            printf("%s\n", longest);
+            printf("len = %d\n", max);
+        }
+    }
+    printf("\n");
     return 0;
 }
 /* my_getline: read a line into s, return length */
 int my_getline(char s[], int lim)
 {
     int c, i;
-    for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
+    for (i = 0; i < lim - 1 && ((c = getchar()) != EOF && c != '\n'); ++i)
         s[i] = c;
     if (c == '\n')
     {
@@ -40,6 +55,7 @@ int my_getline(char s[], int lim)
 void copy(char to[], char from[])
 {
     int i;
+
     i = 0;
     while ((to[i] = from[i]) != '\0')
         ++i;
