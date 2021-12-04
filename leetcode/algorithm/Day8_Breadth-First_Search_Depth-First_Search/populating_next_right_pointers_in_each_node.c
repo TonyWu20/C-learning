1246c57b17d9959b30a89262724a57814d0ff24e
TonyWu20 <tony.w21@gmail.com>
2021-12-04 15:44:02 +0800

    Day 8

---
+++ leetcode/algorithm/Day8_Breadth-First_Search_Depth-First_Search/populating_next_right_pointers_in_each_node.c
@@ -0,0 +1,29 @@
+#include <stdint.h>
+#include <stdio.h>
+struct Node
+{
+    int val;
+    struct Node *left;
+    struct Node *right;
+    struct Node *next;
+};
+
+void connect_left_to_right(struct Node *left, struct Node *right)
+{
+    if (left && right)
+    {
+        left->next = right;
+        connect_left_to_right(left->right, right->left);
+    }
+}
+
+struct Node *connect(struct Node *root)
+{
+    if (root)
+    {
+        connect_left_to_right(root->left, root->right);
+        connect(root->left);
+        connect(root->right);
+    }
+    return root;
+}
