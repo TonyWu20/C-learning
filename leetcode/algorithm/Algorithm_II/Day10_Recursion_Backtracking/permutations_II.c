fc2bb7d7719b0334d46449824552be912f4b764f
TonyWu20 <tony.w21@gmail.com>
2022-01-09 18:22:32 +0800

    Revision

--- leetcode/algorithm/Algorithm_II/Day10_Recursion_Backtracking/permutations_II.c
+++ leetcode/algorithm/Algorithm_II/Day10_Recursion_Backtracking/permutations_II.c
@@ -15,14 +15,14 @@ int factorial(int n)
         return n * factorial(n - 1);
 }
 
-void btPermute(int *nums, int numsSize, int start, int *used, int *tmp,
+void btPermute(int *nums, int numsSize, int start, int *used, int *temp,
                int **result, int *rcs, int *idx)
 {
     if (start == numsSize)
     {
         rcs[*idx] = numsSize;
         result[*idx] = malloc(sizeof(int) * numsSize);
-        memcpy(result[*idx], tmp, sizeof(int) * numsSize);
+        memcpy(result[*idx], temp, sizeof(int) * numsSize);
         (*idx)++;
         return;
     }
@@ -30,11 +30,11 @@ void btPermute(int *nums, int numsSize, int start, int *used, int *tmp,
     {
         if (!used[i])
         {
-            if (i != start && !used[i - 1] && nums[i] == nums[i - 1])
+            if (i > 0 && !used[i - 1] && nums[i] == nums[i - 1])
                 continue;
             used[i] = 1;
-            tmp[start] = nums[i];
-            btPermute(nums, numsSize, start + 1, used, tmp, result, rcs, idx);
+            temp[start] = nums[i];
+            btPermute(nums, numsSize, start + 1, used, temp, result, rcs, idx);
             used[i] = 0;
         }
     }
@@ -47,10 +47,14 @@ int **permuteUnique(int *nums, int numsSize, int *returnSize,
     int *rcs = malloc(*returnSize * sizeof(int)); // short for returnColumnSizes
     int **result = calloc(*returnSize, sizeof(int *));
     int *used = calloc(numsSize, sizeof(int));
-    int *tmp = malloc(numsSize * sizeof(int));
+    int *temp = malloc(numsSize * sizeof(int));
     int idx = 0;
     qsort(nums, numsSize, sizeof(int), cmp);
-    btPermute(nums, numsSize, 0, used, tmp, result, rcs, &idx);
+    btPermute(nums, numsSize, 0, used, temp, result, rcs, &idx);
+    result = realloc(result, sizeof(int *) * idx);
+    rcs = realloc(rcs, sizeof(int) * idx);
+    free(temp);
+    free(used);
     *returnSize = idx;
     *returnColumnSizes = rcs;
     return result;
