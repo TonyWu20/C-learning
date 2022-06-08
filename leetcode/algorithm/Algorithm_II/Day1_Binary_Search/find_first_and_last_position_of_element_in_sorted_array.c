46f5208c825022a045c65217bee12d4dea588e6a
TonyWu20 <tony.w21@gmail.com>
2022-06-08 12:58:51 +0800

    O(logn) with two binary search

--- leetcode/algorithm/Algorithm_II/Day1_Binary_Search/find_first_and_last_position_of_element_in_sorted_array.c
+++ leetcode/algorithm/Algorithm_II/Day1_Binary_Search/find_first_and_last_position_of_element_in_sorted_array.c
@@ -1,27 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int *searchRange(int *nums, int numsSize, int target, int *returnSize) {
-  int *ret = malloc(sizeof(int) * 2);
-  *returnSize = 2;
-  ret[0] = -1;
-  ret[1] = -1;
-  if (numsSize < 1)
+int lowerBound(int *nums, int numsSize, int target)
+{
+    int left = 0, right = numsSize;
+    while (left < right)
+    {
+        int mid = left + (right - left) / 2;
+        if (target > nums[mid])
+            left = mid + 1;
+        else
+            right = mid;
+    }
+    return left;
+}
+
+int upperBound(int *nums, int numsSize, int target)
+{
+    int left = 0, right = numsSize;
+    while (left < right)
+    {
+        int mid = left + (right - left) / 2;
+        if (target < nums[mid])
+            right = mid;
+        else
+            left = mid + 1;
+    }
+    return right - 1;
+}
+
+int *searchRange(int *nums, int numsSize, int target, int *returnSize)
+{
+    int *ret = malloc(sizeof(int) * 2);
+    *returnSize = 2;
+    ret[0] = -1;
+    ret[1] = -1;
+    if (numsSize < 1)
+        return ret;
+    int lower = lowerBound(nums, numsSize, target);
+    int upper = upperBound(nums, numsSize, target);
+    if (lower > upper)
+        return ret;
+    ret[0] = lower;
+    ret[1] = upper;
     return ret;
-  int left = 0, right = numsSize - 1;
-  while (left <= right) {
-    if (nums[left] < target) {
-      left++;
-    } else if (nums[left] == target) {
-      ret[0] = left;
-    }
-    if (nums[right] > target) {
-      right--;
-    } else if (nums[right] == target) {
-      ret[1] = right;
-    }
-    if (ret[0] != -1 && ret[1] != -1)
-      return ret;
-  }
-  return ret;
 }
